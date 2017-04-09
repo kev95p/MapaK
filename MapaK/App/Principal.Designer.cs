1f88bdb09014a00f88f4a554ee618b36cc687679
Kevin Pleitez <pleitez95geova@gmail.com>
2017-04-09 08:38:37 -0600

    argregado tabla de vedad

---
+++ MapaK/App/Principal.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace App
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTabla = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtNumeroVariables = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvTabla
+            // 
+            this.dgvTabla.AllowUserToAddRows = false;
+            this.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabla.Location = new System.Drawing.Point(48, 37);
+            this.dgvTabla.Name = "dgvTabla";
+            this.dgvTabla.Size = new System.Drawing.Size(257, 487);
+            this.dgvTabla.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvTabla);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 560);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tabla de Verdad";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtNumeroVariables);
+            this.groupBox2.Location = new System.Drawing.Point(411, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(617, 90);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtNumeroVariables
+            // 
+            this.txtNumeroVariables.Location = new System.Drawing.Point(141, 40);
+            this.txtNumeroVariables.Name = "txtNumeroVariables";
+            this.txtNumeroVariables.Size = new System.Drawing.Size(42, 20);
+            this.txtNumeroVariables.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero de Variables";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 584);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Principal";
+            this.Text = "Mapa de Karnaugh";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTabla;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumeroVariables;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MapaK/App/Principal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Principal : Form
+    {
+        private int Nvariables = 0;
+        public Principal()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void generarColumnas()
+        {
+            Nvariables = Convert.ToInt32(txtNumeroVariables.Text);
+            int codigoletra = 65;
+
+            dgvTabla.ColumnCount = Nvariables + 1;
+            for(int i = 0; i < Nvariables; i++)
+            {
+                dgvTabla.Columns[i].Name = ((char)codigoletra).ToString();
+                dgvTabla.Columns[i].Width = 30;
+                codigoletra++;
+            }
+            dgvTabla.Columns[Nvariables].Name = "X";
+            dgvTabla.Columns[Nvariables].Width = 30;
+        }
+
+        private void generarFilas()
+        {
+            int numeroFilas = Convert.ToInt32(Math.Pow(2, Nvariables));
+            dgvTabla.Rows.Add(numeroFilas);
+            int potencia = Nvariables - 1;
+
+            for (int i = 0; i < dgvTabla.ColumnCount-1; i++)
+            {
+                int contador = 0;
+                for(int j = 0; j < numeroFilas;j++)
+                {
+                    if (contador == Convert.ToInt32(Math.Pow(2, potencia)) * 2)
+                    {
+                        contador = 0;
+                    }
+                    if (contador < Convert.ToInt32(Math.Pow(2, potencia)))
+                    {
+                        dgvTabla.Rows[j].Cells[i].Value = 0;
+                    }
+                    else
+                    {
+                        dgvTabla.Rows[j].Cells[i].Value = 1;
+                        dgvTabla.Rows[j].Cells[i].Style.BackColor = Color.LightBlue;
+
+                    }
+
+                        contador++;
+                    
+                }
+                potencia--;
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgvTabla.RowCount = 0;
+            generarColumnas();
+            generarFilas();
+        }
+    }
+}
